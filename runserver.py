9c85b0e383c41ae799c418238b1cd3636beff401
Vijay Velagapudi <acchi369@gmail.com>
2016-03-31 17:24:34 +0000

    init repo

---
+++ runserver.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify, make_response, render_template
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+# use this method to get questions
+@app.route('/', methods=['GET'])
+def index():
+   return render_template("index.html")
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=8081)
