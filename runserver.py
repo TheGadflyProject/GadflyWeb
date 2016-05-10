ece675d0449ec9428d1b6f45e1bf4b7bf52bd89d
= <=>
2016-05-10 18:33:07 +0000

    make h8 404 page

--- runserver.py
+++ runserver.py
@@ -9,14 +9,9 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/h8', methods=['GET'])
-def hate():
-    return render_template("hate.html")
-
-
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return render_template("hate.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8081)
